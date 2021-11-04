b2d339219985452290e4b005464ae1306f4804ec
jwUm <87863185+umjiwoo@users.noreply.github.com>
2021-11-04 23:31:43 +0900

    Apply .gitignore

--- api-server-node-js-final/config/secret.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-    'jwtsecret' :  'winnus_jwt_secret_key_ver2021',
-    'TWILIO_ACCOUNT_SID' : 'AC179b9875fb8d84e19808abf751950aa3',
-    'TWILIO_AUTH_TOKEN' : '32846bb6be5d32e707d42964e0f92295',
-};
