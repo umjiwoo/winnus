5514265619ff6e0ccc13c45be7dd066013612b3e
jwUm <87863185+umjiwoo@users.noreply.github.com>
2021-11-05 20:52:06 +0900

    푸드 페어링 정보 가져오는 부분 추가

--- api-server-node-js-final/src/app/Wine/wineProvider.js
+++ api-server-node-js-final/src/app/Wine/wineProvider.js
@@ -27,11 +27,13 @@ exports.retrieveWineInfo=async function(wineId){
     const connection=await pool.getConnection(async (conn) => conn);
     const wineInfo=await wineDao.selectWineInfo(connection,wineId);
     const flavor=await wineDao.selectWineFlavor(connection,wineId);
-    //TODO 푸드 페어링 가져오기--저장하기 애매해서 미뤄둠
+    const pairingFood=await wineDao.selectPairingFood(connection,wineId);
     const reviews=await wineDao.selectWineReviewLimit3(connection,wineId);
     //같은 타입 와인 베스트 가져오기
     const wineType=wineInfo[0].typeId;
     const wineListByType=await wineDao.selectBestWineListByType(connection,wineType,wineId);
+    //TODO 비슷한 와인 가져오기
+
     connection.release();
-    return response(baseResponse.SUCCESS,[{wineInfo:wineInfo}].concat({flavor:flavor}).concat({reviews:reviews}).concat({bestWineByType:wineListByType}));
+    return response(baseResponse.SUCCESS,[{wineInfo:wineInfo}].concat({flavorList:flavor}).concat({pairingFoodList:pairingFood}).concat({reviews:reviews}).concat({bestWineListByType:wineListByType}));
 };
--- api-server-node-js-final/src/app/Wine/wineRoute.js
+++ api-server-node-js-final/src/app/Wine/wineRoute.js
@@ -5,6 +5,6 @@ module.exports = function(app){
     //와인 리스트 가져오기 api-query string
     app.get('/app/wines',wine.getWineList);
 
-    //TODO 와인 상세 페이지
+    //와인 상세 페이지 api
     app.get('/app/wines/:wineId',wine.getWineInfo);
 };
