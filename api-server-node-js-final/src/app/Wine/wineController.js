2e0aae47077d7a2b737922d6299a65b24424f7e4
jwUm <87863185+umjiwoo@users.noreply.github.com>
2021-11-05 21:26:09 +0900

    단순 수정

--- api-server-node-js-final/src/app/Wine/wineController.js
+++ api-server-node-js-final/src/app/Wine/wineController.js
@@ -1,6 +1,6 @@
 const jwtMiddleware = require("../../../config/jwtMiddleware");
 const wineProvider = require("../../app/Wine/wineProvider");
-const wineService = require("../../app/wine/wineService");
+const wineService = require("../../app/Wine/wineService");
 const baseResponse = require("../../../config/baseResponseStatus");
 const {response, errResponse} = require("../../../config/response");
 
